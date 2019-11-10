6046c73274581a0b6894a92fef012ec256ffb587
Ben Dolmar <bdolmar@gmail.com>
2019-11-10 13:34:09 -0600

    Refactored line following logic to be used as a part of missions.

--- main.py
+++ main.py
@@ -10,24 +10,14 @@ from pybricks.robotics import DriveBase
 from simple_pid import PID
 
 from sandwichbot import SandwichBot
+from behaviors.follow_line_behavior import FollowLineBehavior
 
 # Write your program here
 bot = SandwichBot()
 
-max_reflection = 94
-min_reflection = 12
-target_reflection = (max_reflection - min_reflection) / 2
-stop_watch = StopWatch()
-pid = PID(-1.0, -.25, 0.0, target_reflection, 0.01)
-pid.output_limits = (-45, 45)
-last_angle = 0
+# while True:
+#     print("Angle: ", bot.gyro_sensor.angle())
+#     wait(500)
 
-last_angle = 0
-
-while not any(brick.buttons()):
-    reflection = bot.color_sensor.reflection()
-    angle = pid(reflection)
-    if last_angle != angle:
-        bot.drive(50, angle)
-        last_angle = angle
-        print(stop_watch.time(), ' -- reflection:', reflection, ', angle: ', angle, ', error: ', (reflection-target_reflection))
+follow_line = FollowLineBehavior(bot, bot.color_sensor)
+follow_line.run(lambda: any(brick.buttons()))
--- sandwichbot.py
+++ sandwichbot.py
@@ -1,14 +1,20 @@
 from pybricks import ev3brick as brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.ev3devices import (Motor, ColorSensor, GyroSensor)
 from pybricks.parameters import (Port, Stop, Direction, Button, Color,
                                  SoundFile, ImageFile, Align)
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
+
+from behaviors.follow_line_behavior import FollowLineBehavior
 
 class SandwichBot(DriveBase):
     def __init__(self, left_motor=Motor(Port.A), right_motor=Motor(Port.B), wheel_diameter=56, axle_track=114):
         self.left_motor = left_motor
         self.right_motor = right_motor
         self.color_sensor = ColorSensor(Port.S2)
+        self.gyro_sensor = GyroSensor(Port.S1)
         super().__init__(left_motor, right_motor, wheel_diameter, axle_track)
+
+    def follow_line(self, until):
+        follow_line = FollowLineBehavior(self, self.color_sensor)
+        follow_line.run(until)
