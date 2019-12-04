3a388b09c1f5032d291b7fd4205a13672cf91e48
Ben Dolmar <bdolmar@gmail.com>
2019-12-03 20:03:10 -0600

    Removed ramp mission and added build 2 mission.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ bot = SandwichBot()
 # Clear the display
 brick.display.clear()
 
-missions = [SwingMission, BuildMission, CraneMission, RampMission]
+missions = [SwingMission, CraneMission, BuildMission, Build2Mission]
 selected_mission_index = 0
 current_mission_index = 0
 brick.display.clear()
--- missions/build2_mission.py
+++ missions/build2_mission.py
@@ -2,13 +2,13 @@ from pybricks import ev3brick as brick
 from pybricks.tools import print, wait, StopWatch
 from sandwichbot import SandwichBot
 
-class RampMission():
-    IMAGE_PATH = "assets/ramp.jpg"
+class Build2Mission():
+    IMAGE_PATH = "assets/build2.jpg"
 
     def __init__(self, bot:SandwichBot):
-        self.image_path = RampMission.IMAGE_PATH
+        self.image_path = Build2Mission.IMAGE_PATH
         self.bot = bot
 
     def run(self):
-        brick.sound.beep()       
-        print("Run Ramp Mission")
+        brick.sound.beep()
+        
